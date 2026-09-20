504033ed25e98c94d3e0001cec806436eb11f0d0
kairosscommand-blip <kairosscommand@gmail.com>
2026-09-20 02:05:33 -0700

    Delete src/main directory

--- src/main/java/com/nexuscraft/nexuscraftersguild/LootItem.java
+++
@@ -1,7 +0,0 @@
-package com.nexuscraft.nexuscraftersguild;
-
-import org.bukkit.Material;
-
-/** One weighted entry in a loot pool -- see {@link GuildEventLootTable}. */
-record LootItem(Material material, int min, int max, int weight) {
-}
